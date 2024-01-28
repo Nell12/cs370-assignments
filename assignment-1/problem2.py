45ed7293ccaf9cd6f64820175dd376a218d44b79
Aileen Ni <aileenni2003@gmail.com>
2024-01-28 15:24:44 -0500

    Update problem2.py

--- assignment-1/problem2.py
+++ assignment-1/problem2.py
@@ -22,3 +22,16 @@ U, E, V= svd(cov)
 print(U)
 print(E)
 print(V)
+
+
+xx, yy = np.meshgrid(range(10), range(10))
+z = (9-xx-yy)/2
+print(xx)
+print(yy)
+print(z)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(xx, yy, z, alpha=0.2)
+
+plt.show()
