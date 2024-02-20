f0a631b8e2436eb08afc80c7ccb897692f6986d8
Aileen Ni <aileenni2003@gmail.com>
2024-02-20 18:20:15 -0500

    Video Search

---
+++ Video Search/README.md
@@ -0,0 +1,5 @@
+# Aileen Ni
+## Video Search Assignment
+## Date of Submission: March 4 2024
+## Instructions on how to run the code: 
+### The code is run in python using pytube
---
+++ Video Search/video.py
@@ -0,0 +1,31 @@
+from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi as transcript
+
+def download_captions(url, path):
+   code= url.split("v=")[-1] #get URL idcode for video
+   
+   #get transcript
+   srt= transcript.get_transcript(code, languages=['en'])
+   
+   #write captions to file, including starting time and duration
+   with open(f'{path}/{code}.txt', "w") as f:
+      for i in srt:
+         f.write("{}\n".format(i))
+
+
+def download_video(url, path='./Videos'):
+   video= YouTube(url) #gets the url
+
+   video_stream= video.streams.get_highest_resolution()
+   video_stream.download(path) #downloads to file path
+   
+   download_captions(url, path) #downloads caption file
+
+
+#function to download video
+url= ["https://www.youtube.com/watch?v=wbWRWeVe1XE",
+      "https://www.youtube.com/watch?v=FlJoBhLnqko",
+      "https://www.youtube.com/watch?v=Y-bVwPRy_no"]
+
+for i in url:
+   download_video(i)
