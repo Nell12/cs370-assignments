cd8312a05b075f66931e2b6f6dcd6a1bfacdcda5
Aileen Ni <aileenni2003@gmail.com>
2024-02-21 11:19:51 -0500

    Video Library

--- Video Search/VideoLibrary/video.py
+++ Video Search/VideoLibrary/video.py
@@ -1,14 +1,16 @@
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi as transcript
+import re
 
-def download_captions(url, path):
+def download_captions(url, path, title):
    code= url.split("v=")[-1] #get URL idcode for video
-   
+   title= re.sub('[^A-Za-z0-9 ]+', '', title)
+
    #get transcript
    srt= transcript.get_transcript(code, languages=['en'])
    
    #write captions to file, including starting time and duration
-   with open(f'{path}/{code}.txt', "w") as f:
+   with open(f'{path}/{title}.txt', "w") as f:
       for i in srt:
          f.write("{}\n".format(i))
 
@@ -24,8 +26,7 @@ def download_video(url, path='./Videos'):
    video_stream= video.streams.get_highest_resolution()
    video_stream.download(path) #downloads to file path
    
-   download_captions(url, path) #downloads caption file
-
+   download_captions(url, path, video.title)
 
 #function to download video
 url= ["https://www.youtube.com/watch?v=wbWRWeVe1XE",
