5ff3bed06879f764d2970ff0e5365a82c50236e2
Aileen Ni <aileenni2003@gmail.com>
2024-03-02 23:29:35 -0500

    Video Indexing

--- Video Search/README.md
+++ Video Search/README.md
@@ -5,5 +5,6 @@
 ### The first problem of the assignment is in the VideoLibrary directory 
 #### The file video.py is run in python using pytube and youtube_transcript_ai.
 ### The second part of the assignment is in the VideoIndexing directory
-#### File 2.1_decode.py breaks apart a video into frames by fps, and file 2.1_sample.py preprocess the frames and outputs them into a .npy file. The librarues used are cv2, os, and PIL libraries
-#### File 2.2_detecting.py detects the objects in each frame, and uses the additional library tensorflow, and the ssd directory as the pre-trained model.
+#### File 2.2_extract.py breaks apart the video into frames by fps and stores them in the Frames directory (order is scrambled when uploaded to github for unknown reason), and file 2.1_sample.py preprocess the frames and outputs them into a .npy file. The librarues used are cv2, os, numpy, matplotlib and PIL libraries
+#### File 2.2_detecting.py detects the objects in each frame, and uses the additional library tensorflow, and the ssd directory as the pre-trained model. It outputs the dections into 2.2_obj.txt
+#### File 2.3_embedding.py trains the autoencoder using an additional library keras (on top of already mentioned libraries). The training result is outputted into vector_emb[1,2].png images.
--- Video Search/VideoIndexing/2.3_embedding.py
+++ Video Search/VideoIndexing/2.3_embedding.py
@@ -5,18 +5,7 @@ from PIL import Image
 from matplotlib import pyplot as plt
 
 #size of encoded representation
-
-'''
-#"encoded" representation of input
-encoded = layers.Dense(128, activation='relu')(input_img)
-encoded = layers.Dense(64, activation='relu')(encoded)
-encoded = layers.Dense(32, activation='relu')(encoded)
-
-decoded = layers.Dense(64, activation='relu')(encoded)
-decoded = layers.Dense(128, activation='relu')(decoded)
-decoded = layers.Dense(size, activation='sigmoid')(decoded)
-'''
-
+#(52,52,1)
 input_img = keras.Input(shape=(52,52,1))
 
 x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
@@ -43,8 +32,9 @@ autoencoder= keras.Model(input_img, decoded)
 optimizer= keras.optimizers.Adam(learning_rate=0.001)
 autoencoder.compile(optimizer=optimizer, loss='mean_squared_error')
 
+#Open file detection text
 file=open("2.2_obj.txt", "r")
-
+#extract data
 infos=[]
 for line in file:
     info=[]
@@ -63,13 +53,14 @@ for line in file:
 
 target_size= (52,52)
 
+#Extract part of the frames where the objects are found
 images=[]
 for i in infos:
     image_path=f'Frames/{i[0]}.jpg'
 
     img=Image.open(image_path)
     img= img.convert('L')
-
+    #Crop the frame
     xmin, xmax, ymin, ymax= i[1], i[2], i[3], i[4]
 
     crop= img.crop((xmin, ymin, xmax, ymax))
@@ -80,11 +71,11 @@ for i in infos:
     #print(image.size)
 
     images.append(image)
-
+#Final object images for training
 final= np.array(images)
 np.random.shuffle(final)
 #print(final)
-
+#Split the data
 split= int(np.floor(len(final)*0.9))
 x_train, x_test= np.split(final, [split], axis=0)
 x_train= x_train[:,:,:,np.newaxis]
@@ -92,8 +83,6 @@ x_test= x_test[:,:,:,np.newaxis]
 
 #print(x_train)
 #print(x_test)
-
-
 
 #Train autoencoder for 200 epochs
 autoencoder.fit(x_train, x_train,
